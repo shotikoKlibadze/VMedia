bc0b32290116e8a4ea47387428c16df119b4d756
Shota <sh.klibadze@gmail.com>
2023-02-28 11:00:56 +0400

    Initial Commit

---
+++ VMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VMedia
+//
+//  Created by Shotiko Klibadze on 28.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
